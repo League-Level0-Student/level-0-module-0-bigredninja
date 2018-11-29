a2b5502383d004503a9fccdf840a36d9f17b0a30
ilabstaff <ilabstaff@172.16.116.68>
2018-11-28 17:55:42 -0800

    Robot square

--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -4,6 +4,8 @@ package strings_and_dialogs;
  *    Level 0
  */
 
+import javax.print.DocFlavor.INPUT_STREAM;
+
 import org.jointheleague.graphical.robot.Robot;
 
 // This recipe draws a square using the Robot
@@ -12,22 +14,43 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+     Robot  zack=new Robot();
 
         // 3. Put the robot's pen down
-
-
+       zack.penDown();
+     
         // 6. Make the robot move as fast as possible
-
-
-        // 5. Do everything below here 4 times
-
+   
+zack.setSpeed(1000);
+       
+		
+	
 
         //         2. Move your robot 200 pixels
-
-
+    	zack.move(300);   https://github.com/League-Level0-Student/level-0-module-0-bigredninja.git zack.Draw half square();
+          
+    		 
+    		 
+    		 
+    		 
+    		 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+        zack.turn(90);
 
-
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
     }
 }
