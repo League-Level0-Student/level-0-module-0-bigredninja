cdc5b7664bea1cc4502621b7a28c7023e648f49a
ilabstaff <ilabstaff@172.16.116.68>
2018-12-05 19:17:54 -0800

    mogul0

---
+++ src/EveryThinIsAwsome.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class EveryThinIsAwsome {
+	public static void main(String[] args) {
+//		 Create a class called EverythingIsAwesome
+//		 2. Add a main method
+//		 3. Use JOptionPane.showInputDialog to ask them what they like, e.g. What do you like?
+//		 HINT: Use a variable to store their answer, e.g. space
+//		 4. Use JOptionPane.showMessageDialog to tell them it is awesome, e.g. space is awesome
+//
+//		 Copyright League of Amazing Programmers 2013-2018
+String red = JOptionPane.showInputDialog("what is your favorite animal");
+	
+JOptionPane.showMessageDialog(null,  "The grinch ate"  +red);
+	
+	
+	}
+	
+}
